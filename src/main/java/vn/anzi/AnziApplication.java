bcb06b9c0ed590b1942d51c1f71324512f0323a2
unknown <phamhuuthanhdat1992@yahoo.com.vn>
2022-08-20 12:20:19 +0700

    init

---
+++ src/main/java/vn/anzi/AnziApplication.java
@@ -0,0 +1,13 @@
+package vn.anzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnziApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/anzi/AnziApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.anzi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnziApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
